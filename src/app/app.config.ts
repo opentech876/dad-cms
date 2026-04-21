6268c1784918d923a9f010289ea89f3c11b8b4d5
MrOlmb <elvisolembe1@gmail.com>
2026-04-21 05:22:29 +0100

    chore:Taiga-UI setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTaiga } from '@taiga-ui/core';
 
 import { routes } from './app.routes';
 
@@ -7,6 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideTaiga(), //provide taiga elements to the app
+  ],
 };
