6cba6bf4577665d7fb2eadbcca750de1ba906270
MrOlmb <elvisolembe1@gmail.com>
2026-04-21 11:04:32 +0100

    chore: Added supabase connections

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://dttqbktyqhzdmimzkoeb.supabase.co/rest/v1/',
+  supabaseKey: 'sb_publishable_8Vv5Wt6fxFj-FwXOlP8WbA_EMwuclFo',
+};
