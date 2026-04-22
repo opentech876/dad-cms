166cf58165c49a43788bbd1003cfa36051068a9b
opentech876 <opentech876@gmail.com>
2026-04-22 04:05:56 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  supabaseUrl: 'https://dttqbktyqhzdmimzkoeb.supabase.co/rest/v1/',
-  supabaseKey: 'sb_publishable_8Vv5Wt6fxFj-FwXOlP8WbA_EMwuclFo', //can be changed without downtime or jwt reset for end users
+  supabaseUrl: '',
+  supabaseKey: '', //can be changed without downtime or jwt reset for end users
 };
